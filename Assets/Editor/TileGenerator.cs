afe644aae293c37e49b3062295a9148c44d4932e
Alberto Paz Becerril <alberto.paz@mediatonic.co.uk>
2018-12-12 18:31:01 +0100

    Add character model.

--- Assets/Editor/TileGenerator.cs
+++ Assets/Editor/TileGenerator.cs
@@ -10,11 +10,17 @@ using Random = System.Random;
 
 public class TileGenerator : UnityEditor.ScriptableWizard
 {
+    private const float ColumnsCenterDistances = 1.73f;
+    private const float RowsCenterDistances = 1.5f;
+
     [SerializeField]
     private int _width = 0;
 
     [SerializeField]
     private int _height = 0;
+
+    private Random _random;
+    private List<string> _hexagonPrefabsResourcePaths;
 
     [MenuItem("Tools/Generate tiles")]
     static void TileGeneratorWizard()
@@ -24,26 +30,15 @@ public class TileGenerator : UnityEditor.ScriptableWizard
 
     void OnWizardCreate()
     {
-        const float ColumnsCenterDistances = 1.73f;
-        const float RowsCenterDistances = 1.5f;
-
         float x = 0;
         float y = 0;
 
-        var hexagonPrefabsResourcePaths = GetHexagonsPrefabsResourcePaths().ToList();
-
-        var random = new Random();
+        _hexagonPrefabsResourcePaths = GetHexagonsPrefabsResourcePaths().ToList();
+        _random = new Random();
 
         for (var i = 0; i < _width; i ++)
         {
-            // paint column
-            for (var j = 0; j < _height; j++)
-            {
-                var prefabIndex = random.Next(0, hexagonPrefabsResourcePaths.Count);
-                Instantiate(Resources.Load(hexagonPrefabsResourcePaths[prefabIndex], typeof(GameObject)), new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0));
-                
-                y += RowsCenterDistances * 2f;
-            }
+            PaintColumn(x, y);
 
             if (i % 2 == 0)
             {
@@ -72,5 +67,26 @@ public class TileGenerator : UnityEditor.ScriptableWizard
         var hexagonPrefabsFileNames = hexagonPrefabFilePaths.Select(f => Path.GetFileName(f).Replace(".prefab", ""));
 
         return hexagonPrefabsFileNames.Select(fileName => ResourcesPath + "/" + fileName);
+    }
+
+    private void PaintColumn(float x, float startingY)
+    {
+        var y = startingY;
+
+        for (var j = 0; j < _height; j++)
+        {
+            PaintCell(x, y);
+
+            y += RowsCenterDistances * 2f;
+        }
+    }
+
+    private void PaintCell(float x, float y)
+    {
+        var prefabIndex = _random.Next(0, _hexagonPrefabsResourcePaths.Count);
+        Instantiate(
+            Resources.Load(_hexagonPrefabsResourcePaths[prefabIndex], typeof(GameObject)),
+            new Vector3(x, 0, y),
+            Quaternion.Euler(90, 0, 0));
     }
 }
