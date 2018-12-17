4f935528de264f7cbee9288e86d84abbbfe22a45
Alberto Paz Becerril <alberto.paz@mediatonic.co.uk>
2018-12-17 09:33:05 +0100

    Tests with selected shader.

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -4,6 +4,10 @@ namespace Assets.Scripts.Map
 {
     public class Tile : MonoBehaviour
     {
+        private Shader _originalShader;
+        private Shader _selectedShader;
+        private Renderer _renderer;
+
         public Vector3 Center { get; set; }
 
         public bool IsUncovered { get; private set; }
@@ -11,6 +15,9 @@ namespace Assets.Scripts.Map
         private void Awake()
         {
             IsUncovered = false;
+            _originalShader = GetComponent<Renderer>().material.shader;
+            _selectedShader = Shader.Find("Outlined/Silhouetted Diffuse");
+            _renderer = GetComponent<Renderer>();
         }
 
         public void Uncover()
@@ -18,6 +25,16 @@ namespace Assets.Scripts.Map
             transform.Rotate(Vector3.down, 180);
 
             IsUncovered = true;
+        }
+        
+        private void OnMouseOver()
+        {
+            _renderer.material.shader = _selectedShader;
+        }
+
+        private void OnMouseExit()
+        {
+            _renderer.material.shader = _originalShader;
         }
     }
 }
